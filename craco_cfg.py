81c3ca39665b127db1e3d9436d368f61e7050834
Andywang201605 <ztwang201605@gmail.com>
2023-08-03 13:06:25 +1000

    add autoflagger

--- craco_cfg.py
+++ craco_cfg.py
@@ -8,9 +8,15 @@ THRESHOLD           =       6 # number of threshold to be used in the search pip
 DEAD                =       None # "seren-02:0-1" #"seren-01:0-1,seren-04:0-1,seren-07:1,seren-08:1" # "seren-01:0-1,seren-04:1,seren-07:1"
 FLAGANT             =       "24-30" # additional antenna to be flagged, should be a string (should be 1-based)
 SUBTRACT            =       64 # subtract every 32 samples
-FLAGCHAN            =       "47-55" # channel number to be flagged
+FLAGCHAN            =       "60-84" #"47-55" for VAST observation # channel number to be flagged
 UPDATE_UV_BLOCKS    =       768 # can be 0, or anything above 256 (included)
 SAVE_UVFITS         =       False # whether to save the uvfits files or not
+
+# flagger related parameters
+AUTOFLAGGER         =       True # turn on/off the auto IQRM flagger
+DFLAG_FRADIUS        =       64 # suggested by Vivek, 64
+DFLAG_THRESH        =       5 # threshold for flagging bad channels
+DFLAG_TBLK          =       16 # caluclate statistics over how many samples
 
 # some usually not changing parameter here
 FCM                 =       "/home/ban115/20220714.fcm"
