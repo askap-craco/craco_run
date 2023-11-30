32c52092da776cf7fa8de4ac74af29c4706d8592
Andywang201605 <ztwang201605@gmail.com>
2023-11-30 12:18:20 +0800

    clean up space for skadi

--- fixsbid_uvfits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### fix uvfits files for a given sbid
-sbid=$1
-
-for file in /data/seren-??/big/craco/$sbid/scans/??/*/*/b??.uvfits
-do
-    fixuvfits $file
-done
--- mpiexamcard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mpirun --hostfile mpi_seren.txt --map-by ppr:2:node -x PATH -x XILINX_XRT /data/seren-01/big/craco/wan342/craco_run/examcards.sh
