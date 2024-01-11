0ce6b2eef07465f08dc88aa0ff2f4e1d25fcecf4
CRAFT operator <craftop@skadi-00.atnf.CSIRO.AU>
2024-01-11 14:50:33 +0800

    scripts for deleting files

---
+++ delete_sbfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Deletes uvfits files in the givieng local directory on all nodes in the $HOSTFILE
+# input should be a list of directories like /data/craco/craco/SB012345
+# use it like:
+# ls -d /data/craco/craco/SB055699 | xargs ./delete_sbfile.sh '*.uvfits'
+pattern=$1
+shift
+
+for f in $@ ; do
+    keepfile=$f/KEEP
+    if [[ -f $keepfile ]] ; then
+       echo "SBDIR $f has keepfile. Not deleting. Keepfile has"
+       cat $keepfile
+    else
+	echo "Deleting UVFITS file in $f hostfile=$HOSTFILE"
+	mpirun -hostfile $HOSTFILE --map-by ppr:1:node `which find` $f -name $pattern -delete
+    fi
+done
